e1babb008a9c860f8c91dedad0b138506c0e5a60
hyeonwowo <tlsgusdn0806@naver.com>
2025-01-31 18:58:48 +0900

    COMMIT

---
+++ 프론트엔드/JS/연습장.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript 예제</title>
+</head>
+<body>
+    <button onclick="showAlert()">클릭하세요</button>
+
+    <script>
+        function showAlert() {
+            alert("버튼이 클릭되었습니다!");
+        }
+    </script>
+</body>
+</html>
