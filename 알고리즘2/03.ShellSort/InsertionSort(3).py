7acbc05b715020e642afe26aa8275151598cec8c
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-12 15:30:55 +0900

    Sortion

--- 알고리즘2/03.ShellSort/InsertionSort(3).py
+++ 알고리즘2/03.ShellSort/InsertionSort(3).py
@@ -1,8 +1,10 @@
+# 역순으로 삽입 정렬
+
 def insertionSort(a):    
     for i in range(1,len(a)):
         key = a[i]
         j = i-1
-        while j>=0 and a[j]>key:
+        while j>=0 and a[j]<key:
             a[j+1] = a[j]
             j -= 1
         a[j+1] = key
@@ -10,5 +12,7 @@ def insertionSort(a):
     
 if __name__ == "__main__":
     print(insertionSort([5, 1, 3, 2]))
+    print(insertionSort([1,3,2,4,3,5,4,6,5,7,6,8,9,10]))
+
     print(insertionSort(["b", "f", "z", "d", "i", "k", "p", "v"]))
     #insertionSort([7,2,4,1,9,0])
--- 알고리즘2/03.ShellSort/SelectionSort(3).py
+++ 알고리즘2/03.ShellSort/SelectionSort(3).py
@@ -1,13 +1,15 @@
+# 역순으로 선택정렬
 def selectionSort(a):    
     for i in range(len(a)-1):
-        min_idx = i
+        max_idx = i
         for j in range(i+1,len(a)):
-            if a[min_idx] > a[j]:
-                min_idx = j
-        a[i],a[min_idx] = a[min_idx],a[i]
+            if a[max_idx] < a[j]:
+                max_idx = j
+        a[i],a[max_idx] = a[max_idx],a[i]
     return a
     
 if __name__ == "__main__":
     print(selectionSort([5, 1, 3, 2]))
+    print(selectionSort([1,3,2,4,3,5,4,6,5,7,6,8,7,9,10]))
     print(selectionSort(["b", "f", "z", "d", "i", "k", "p", "v"]))
     #selectionSort([7,2,4,1,9,0])
