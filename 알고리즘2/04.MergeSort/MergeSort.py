322787528b09013ac8ea35240f105d35f3f2e190
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-16 21:23:13 +0900

    a

--- 알고리즘2/04.MergeSort/MergeSort.py
+++ 알고리즘2/04.MergeSort/MergeSort.py
@@ -4,15 +4,20 @@ def merge(a, aux, lo, mid, hi):
     
     i, j = lo, mid+1
     for k in range(lo, hi+1):
-        if i>mid: a[k], j = aux[j], j+1            
-        elif j>hi: a[k], i = aux[i], i+1            
-        elif aux[i] <= aux[j]: a[k], i = aux[i], i+1
-        else: a[k], j = aux[j], j+1            
+        if i>mid: 
+            a[k], j = aux[j], j+1            
+        elif j>hi: 
+            a[k], i = aux[i], i+1            
+        elif aux[i] <= aux[j]: 
+            a[k], i = aux[i], i+1
+        else: 
+            a[k], j = aux[j], j+1            
 
     return a
 
 def divideNMerge(a, aux, lo, hi):
-    if (hi <= lo): return a
+    if (hi <= lo): 
+        return a
     mid = (lo + hi) // 2
     divideNMerge(a, aux, lo, mid)
     divideNMerge(a, aux, mid+1, hi)
