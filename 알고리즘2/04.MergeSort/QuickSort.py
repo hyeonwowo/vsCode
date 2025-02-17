91dd708c4502a723d5e4a89a991edd71144deff5
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-17 20:48:34 +0900

    asdf

--- 알고리즘2/04.MergeSort/QuickSort.py
+++ 알고리즘2/04.MergeSort/QuickSort.py
@@ -2,29 +2,29 @@ import random
 
 # Partition a[lo~hi] using a[lo] as the partitioning (pivot) item
 def partition(a, lo, hi):
-    i, j = lo+1, hi
+    i, j = lo+1, hi # 시작 포인트(1)(2) 설정, i가 기준점보다 큰 요소 담당, j는 기준점보다 작은 요소 담당. 해당하는 요소에 위치하면 대기, i-j 모두 대기상태일 때 스왑
 
     while True:
-        while i <= hi and a[i] < a[lo]: i = i+1
-        while j >= lo+1 and a[j] > a[lo]: j = j-1
+        while i <= hi and a[i] < a[lo]: i = i+1 # 기준점보다 작으면 맞는 위치에 있기에 그대로 두고, 우측 인덱스로
+        while j >= lo+1 and a[j] > a[lo]: j = j-1 # 기준점보다 크면 맞는 위체에 있기에 그대로 두고, 좌측 인덱스로
 
-        if (j <= i): break      # Break if the pointers cross
+        if (j <= i): break      # 두 포인트가 만나서 엇갈리면 종료
         a[i], a[j] = a[j], a[i]   # Swap a[i] and a[j]
-        i, j = i+1, j-1
+        i, j = i+1, j-1 # 스왑 후, 한칸씩 이동
 
-    a[lo], a[j] = a[j], a[lo]    # Swap a[j] with the partitioning item    
-    return j    # Return the index of item in place    
+    a[lo], a[j] = a[j], a[lo]    # j가 기준점보다 작은 요소를 담당하므로 j와 lo(기준점) 스왑   
+    return j    # 기준점이 자리잡은 위치 반환(j와 스왑한 자리 -> j값)
 
 # Partition a[lo~hi] and then continue to partition each half recursively
 def divideNPartition(a, lo, hi):
     if (hi <= lo): return
     j = partition(a, lo, hi)    
-    divideNPartition(a, lo, j-1)
-    divideNPartition(a, j+1, hi)
+    divideNPartition(a, lo, j-1) # 반환된 j를 기준으로 hi값을 잡음
+    divideNPartition(a, j+1, hi) # 반환된 j를 기주으로 lo값을 잡음
 
 def quickSort(a):
     # Randomly shuffle a, so that the partitioning item is chosen randomly
-    random.shuffle(a)
+    random.shuffle(a) # 만약, 요소들이 정렬된 상태에서 quickSort 수행시 n^2 -> random.shuffle(a)를 통해 이와 같은 상황 방지
 
     divideNPartition(a, 0, len(a)-1)
     return a
