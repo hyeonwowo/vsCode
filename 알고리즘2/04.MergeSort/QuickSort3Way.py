006dc51e94c5266bde6d88fe9e099e77c2e795ea
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-24 20:47:42 +0900

    a

--- 알고리즘2/04.MergeSort/QuickSort3Way.py
+++ 알고리즘2/04.MergeSort/QuickSort3Way.py
@@ -3,10 +3,10 @@ import random
 # Partition a[lo~hi] into 3-sections and then continue to partition each half recursively
 def partition3Way(a, lo, hi):
     if (hi <= lo): return
-    v = a[lo]
-    lt, gt = lo, hi  # Indices to put next items <v and >v    
-    i = lo
-    while i <= gt:
+    v = a[lo] # v는 기준점
+    lt, gt = lo, hi  # lt : 피벗보다 작은 값들의 끝을 가르킴. gt : 피벗보다 큰 값들의 시작을 가르킴
+    i = lo # i 는 움직일 포인터
+    while i <= gt: # i가 오른쪽 영역에 도달시 검사 중단
         if a[i] < v:
             a[lt], a[i] = a[i], a[lt]  # Swap
             lt, i = lt+1, i+1
@@ -17,8 +17,8 @@ def partition3Way(a, lo, hi):
 
     print(a)
 
-    partition3Way(a, lo, lt-1)
-    partition3Way(a, gt+1, hi)
+    partition3Way(a, lo, lt-1) # 피벗보다 작은 부분 정렬
+    partition3Way(a, gt+1, hi) # 피벗보다 큰 부분 정렬
 
 def quickSort3Way(a):
     # Randomly shuffle a, so that the partitioning item is chosen randomly
