f9a75a62e15a22fffcbad7e4770498498ec8d065
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-24 15:41:45 +0900

    a

--- 알고리즘2/04.MergeSort/QuickSelect(2).py
+++ 알고리즘2/04.MergeSort/QuickSelect(2).py
@@ -3,26 +3,28 @@ import random
 # Partition a[lo~hi] using a[lo] as the partitioning (pivot) item
 def partition(a, lo, hi):
     i,j = lo+1, hi
-    
+
     while True:
         while i<=hi and a[i]<a[lo]: i=i+1
         while j>=lo+1 and a[j]>a[lo]: j=j-1
 
         if j<=i: break
-        a[lo],a[j] = a[j],a[lo]
-        return j
-    return j    # Return the index of item in place    
+        a[i],a[j]=a[j],a[i]
+        i,j = i+1, j-1
+    a[lo], a[j] = a[j], a[lo]
+    return j
 
 # Find k-th smallest element, where k = 0 ~ len(a)-1
 def quickSelect(a, k):
     random.shuffle(a)
 
-    lo,hi=0,len(a)-1
+    lo,hi = 0, len(a)-1
     while lo<hi:
-        j = partition(a,lo,hi)
+        j=partition(a,lo,hi)
         if j<k: lo=j+1
         elif k<j: hi=j-1
         else: return a[k]
+
     return a[k]
 
 if __name__ == "__main__":
