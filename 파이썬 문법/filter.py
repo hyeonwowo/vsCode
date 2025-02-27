af899a5961595649d529d0b57f01f04ccf0d3e2a
hyeonwowo <tlsgusdn0806@naver.com>
2025-02-27 16:04:57 +0900

    a

--- 파이썬 문법/filter.py
+++ 파이썬 문법/filter.py
@@ -4,7 +4,13 @@ def func(n):
     return n<3
 
 input_list = [1,2,3,4,5,6,7,8,9,10]
-result_list = filter(func, input_list)
+result_list = filter(func, input_list) # True인 것만 새로운 이터레이터 형태로 반환
 
-print(result_list)
-print(list(result_list))
+print(result_list) # <filter object at 0x1022dabe0>
+print(list(result_list)) # [1, 2]
+
+
+# +) map() 사용시
+result_list = map(func, input_list)
+print(result_list) # <map object at 0x102755eb0>
+print(list(result_list)) # [True, True, False, False, False, False, False, False, False, False]
--- 파이썬 문법/upper-lower.py
+++ 파이썬 문법/upper-lower.py
@@ -2,20 +2,20 @@
 
 
 # 소문자 -> 대문자 : result = text.upper()
-
 text = "hello world"
 result = text.upper()
 print(result)  # 출력: HELLO WORLD
 
 
-# 대문자 -> 소문자 : result = text.lower()
 
+# 대문자 -> 소문자 : result = text.lower()
 text = "HELLO WORLD"
 result = text.lower()
 print(result)
 
-# 대문자 -> 소문자 + 소문자 -> 대문자 : result = text.swapcase()
 
+
+# 대문자 -> 소문자 + 소문자 -> 대문자 : result = text.swapcase()
 text = "Hello World"
 result = text.swapcase()
 print(result)  # 출력: hELLO wORLD
