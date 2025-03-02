2e62795a8037ae45e47e2d851c4d628d3b7fc3fe
hyeonwowo <tlsgusdn0806@naver.com>
2025-03-02 19:04:21 +0900

    a

--- 알고리즘2/05.PQ/MaxHeap(3).py
+++ 알고리즘2/05.PQ/MaxHeap(3).py
@@ -1,45 +1,18 @@
 class MaxHeap:
     def __init__(self):
-        self.pq = ['']
+        pass
 
     def insert(self, key):
-        self.pq.append(key)
-        idx = len(self.pq) - 1
-
-        while idx > 1 and self.pq[idx // 2] < key:
-            self.pq[idx], self.pq[idx // 2] = self.pq[idx // 2], self.pq[idx]
-            idx = idx // 2
+        pass
 
     def delMax(self):
-        if len(self.pq) <= 1:
-            raise IndexError("Heap is empty")
-
-        self.pq[1], self.pq[-1] = self.pq[-1], self.pq[1]
-        max_val = self.pq.pop()
-
-        idx = 1
-        while 2 * idx < len(self.pq):
-            left = 2 * idx
-            right = 2 * idx + 1
-
-            if right < len(self.pq) and self.pq[right] > self.pq[left]:
-                larger_child = right
-            else:
-                larger_child = left
-
-            if self.pq[idx] >= self.pq[larger_child]:
-                break
-
-            self.pq[idx], self.pq[larger_child] = self.pq[larger_child], self.pq[idx]
-            idx = larger_child
-
-        return max_val
+        pass
 
     def size(self):
-        return len(self.pq) - 1
+        pass
 
     def isEmpty(self):
-        return len(self.pq) <= 1
+        pass
 
 
 if __name__ == "__main__":    
