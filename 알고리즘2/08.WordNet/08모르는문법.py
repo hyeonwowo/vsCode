2763064ad0a91f75adeb6533a4f13fcd8ed4d6e8
hyeonwowo <tlsgusdn0806@naver.com>
2025-03-18 15:08:51 +0900

    message

--- 알고리즘2/08.WordNet/08모르는문법.py
+++ 알고리즘2/08.WordNet/08모르는문법.py
@@ -1,6 +1,8 @@
 # 해당 페이지에서 모르는 문법 정리
 
-str == "ab"
+str1 = "ab"
 
-if str not in "a":
-    print("not in a")
+if str1 not in "a":
+    print("str not in a")
+elif str1 in "a":
+    print("str in a")
