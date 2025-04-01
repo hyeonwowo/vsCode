0a8f6fa66b8fc8cb6b96835ca72326fa382910ef
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-01 15:42:31 +0900

    a

--- 백준/파이썬/새롭게 알게된.py
+++ 백준/파이썬/새롭게 알게된.py
@@ -1,3 +1,12 @@
 # 한번에 여러 요소 입력받기 - map()
 x, y, z, = map(int, input("").split())
 print(x, y, z)
+
+# sys.stdin.readline().split() - input보다 빠르게 데이터 입력받기
+import sys
+
+line = sys.stdin.readline()
+print(line)
+
+x, y, z = sys.stdin.readline().split()
+print(x, y, z)
