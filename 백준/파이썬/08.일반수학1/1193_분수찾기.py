88d1901de1bd8684726e1435053f5f0716d5a1ef
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-08 23:14:39 +0900

    a

--- 백준/파이썬/08.일반수학1/1193_분수찾기.py
+++ 백준/파이썬/08.일반수학1/1193_분수찾기.py
@@ -1,5 +1,11 @@
-def find(n):
-    
+import math
+
+def find(k):
+    x = math.ceil((-1 + math.sqrt(1 + 8 * k)) / 2)
+    y = 1
+    i = k%x
+    return f"{x-i-1}/{y+i-1}"
 
 if __name__ == "__main__":
-    print(find(int(input())))
+    
+    print(find(k))
