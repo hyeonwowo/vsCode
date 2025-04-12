0fed12570f2df5cfc04e8cbbbb0addaf721f185a
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-12 16:19:46 +0900

    a

--- 백준/파이썬/11.시간복잡도/24313_점근적표기.py
+++ 백준/파이썬/11.시간복잡도/24313_점근적표기.py
@@ -1,15 +1,15 @@
 import sys
 
-def changeTo(fn):
-    f = str(fn)
-    for i in range(0,len(fn),1):
-
-def bigO(fn,c,n0):
-    pass
+def is_big_o(a1, a0, c, n0):
+    for n in range(n0, 101):  # n0 이상 모든 n (문제 조건: n ≤ 100)
+        fn = a1 * n + a0
+        gn = c * n
+        if fn > gn:
+            return 0
+    return 1
 
 if __name__ == "__main__":
-    fn = map(int, sys.stdin.readline().split())
-    c = int(input())
-    n0 = int(input())
-    print(bigO(fn,c,n0))
-    
+    a1, a0 = map(int, sys.stdin.readline().split())
+    c = int(sys.stdin.readline())
+    n0 = int(sys.stdin.readline())
+    print(is_big_o(a1, a0, c, n0))
