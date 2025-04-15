888ad46add63d446e9e22f98300db22b14409a0a
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-15 21:52:57 +0900

    a

--- 백준/파이썬/12.브루트포스/1018_체스판다시칠하기.py
+++ 백준/파이썬/12.브루트포스/1018_체스판다시칠하기.py
@@ -1,25 +1,24 @@
 import sys
 
 def startPoint(lst,n,m):
-    spx, spy = n - 8 + 1, m - 8 + 1
+    spx, spy = n - 8, m - 8
     mincolor = float('inf')
     for x in range(spx+1):
         for y in range(spy+1):
-            colorcount = colorChess(lst[x][y],lst)
-            if mincolor > colorcount:
-                mincolor = colorcount
+            colorcount1 = colorChess(x,y,lst,"W")
+            colorcount2 = colorChess(x,y,lst,"B")
+            mincolor = min(mincolor, colorcount1, colorcount2)
     return mincolor
                 
-def colorChess(color,lst):
+def colorChess(x, y, lst, start_color):
     count = 0
-    for row in range(1,9):
-        for col in range(1,9):
-            if (row + col) % 2 == 0 and lst[row][col] != color:
-                count += 1
-            elif (row + col) % 2 != 0 and lst[row][col] == color:
+    for i in range(8):
+        for j in range(8):
+            expected = start_color if (i + j) % 2 == 0 else ('B' if start_color == 'W' else 'W')
+            if lst[x + i][y + j] != expected:
                 count += 1
     return count
-      
+
 if __name__ == "__main__":
     n,m = map(int, sys.stdin.readline().split())
     lst = [[] for _ in range(n)]
