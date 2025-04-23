304736ead98848175f8ad489c12c13726d118e07
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-23 13:36:29 +0900

    a

--- 백준/파이썬/16.스택,큐,덱/2346_풍선터뜨리기.py
+++ 백준/파이썬/16.스택,큐,덱/2346_풍선터뜨리기.py
@@ -1,2 +1,8 @@
 import sys
 from collections import deque
+
+def Boom(n):
+    pass
+
+if __name__ == "__main__":
+    
--- 백준/파이썬/16.스택,큐,덱/연습장.py
+++ 백준/파이썬/16.스택,큐,덱/연습장.py
@@ -1,2 +1,6 @@
-a = lambda x:x*2
-print(a(2))
+n = int(input())
+
+result = 1
+for i in range(1,n+1):
+    result *= i
+print(result)
