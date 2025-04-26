b6ef97831d6db298857aee5c5054e290e594a775
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-26 12:30:43 +0900

    a

--- 백준/파이썬/18.심화/25192_인사성밝은곰곰이.py
+++ 백준/파이썬/18.심화/25192_인사성밝은곰곰이.py
@@ -1,17 +1,17 @@
 import sys
 
 def hellobear(n):
+    myset = set()
     count = 0
-    userlog = set()
     
-    for _ in range(n):    
-        log = sys.stdin.readline().strip()        
-        if log == "ENTER":
-            count += len(userlog)
-            userlog.clear()
-            
-        userlog.add(log)
-    return count
+    for _ in range(n):
+        data = sys.stdin.readline().strip()
+        if data == "ENTER":
+            count += len(myset)
+            myset.clear()
+        else:
+            myset.add(data)
+    return len(myset) + count
         
 if __name__ == "__main__":
     print(hellobear(int(input())))
---
+++ 백준/파이썬/18.심화/26069_붙임성좋은총총이.py
@@ -0,0 +1,15 @@
+import sys
+
+def chongchong(n):
+    myset = set()
+    myset.add("ChongChong")
+    for _ in range(n):
+        usera, userb = sys.stdin.readline().split()
+        if usera in myset:
+            myset.add(usera)
+        elif userb in myset:
+            myset.add(userb)
+    return len(myset)
+
+if __name__ == "__main__":
+    print(chongchong(int(input())))
