40ad358252a7d450d1867d038e62af0ff71c5d20
hyeonwowo <tlsgusdn0806@naver.com>
2025-04-30 19:26:51 +0900

    a

--- 백준/파이썬/20.백트래킹/9663_NQueen(2).py
+++ 백준/파이썬/20.백트래킹/9663_NQueen(2).py
@@ -1,12 +1,21 @@
 import sys
 
-def backtrack(start):
+def backtrack(k):
     global cnt
-    for i in N:
-        if queen(start, i):
-            
+    for i in range(N):
+        if queen(k,i):
+            board[k] = i
+            if k == N-1:
+                cnt += 1
+                return
+            else:
+                backtrack(k+1)
+
 def queen(a,b):
-    if 
+    for i in range(a):
+        if board[i] == b or abs(board[i]-b) == abs(i-a):
+            return False
+    return True
 
 if __name__ == "__main__":
     N = int(input())
--- 백준/파이썬/20.백트래킹/9663_NQueen.py
+++ 백준/파이썬/20.백트래킹/9663_NQueen.py
@@ -2,24 +2,25 @@ import sys
 
 def backtracking(k):
     global cnt
-    for i in range(N):
-        if queen(k,i): # k번째 행, i번째 열
+    for i in range(N): # 열 번호를 순회
+        if queen(k,i): # k번째 행, i번째 열. queen() 검사를 통과하면, k번째행의 i번째 열에 queen 배치
             board[k] = i
-            if k == N-1:
+            if k == N-1: # 전체 queen의 수가 N-1개라면(0~N-1) cnt리턴
                 cnt += 1
                 return
             else:
-                backtracking(k+1)
+                backtracking(k+1) # 아니라면 다음행으로 이동
 
-def queen(a,b):
+def queen(a,b): # a : 현재 놓으려는 퀸의 "행" 번호. b : 현재 놓으려는 퀸의 "열" 번호
     for i in range(a):
         if board[i] == b or abs(board[i]-b) == abs(i-a): # 대각선 위치의 열에서 현재 퀸의 열을 뺀 값과 대각선 위치의 행에서 현재 퀸의 행을 뺀 값의 절댓값이 같다
+            # board[k] = i 는 k번째 행에 i열에 퀸을 놓았다는 뜻. 즉 board[0] = 1 이면, 0행 1열에 퀸이 있다는 뜻
             return False
     return True
 
 if __name__ == "__main__":
-    N = int(sys.stdin.readline())
+    N = int(sys.stdin.readline()) 
     board = [0]*N
     cnt = 0
-    backtracking(0)
+    backtracking(0) # 0번째 행에서 퀸을 놓을 열을 하나씩 시도
     print(cnt)    
