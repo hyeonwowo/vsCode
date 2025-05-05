67a7362a44c324ca1dc621572424a7bfedfcf571
hyeonwowo <tlsgusdn0806@naver.com>
2025-05-05 15:48:32 +0900

    a

--- 백준/파이썬/22.누적합/새롭게 알게된.py
+++ 백준/파이썬/22.누적합/새롭게 알게된.py
@@ -15,3 +15,16 @@ print()
 # 소문자 a-z 출력
 for i in range(97, 123):
     print(chr(i), end=' ')  # a b c ... z
+
+
+# lst.index()
+lst = [1, 3, 5, 7, 9]
+idx = lst.index(3)
+print(idx)  # 출력: 1
+
+    # 주의사항 : lst.index(x)는 리스트에 x가 없으면 ValueError 예외를 발생, in 연산자를 먼저 확인
+if 3 in lst:
+    idx = lst.index(3)
+    print(idx)
+else:
+    print("값이 리스트에 없습니다.")
--- 백준/파이썬/22.누적합/연습장.py
+++ 백준/파이썬/22.누적합/연습장.py
@@ -1,17 +1,16 @@
 import sys
 
-starttime = [i for i in range(0,25)]
-endtime = [i for i in range(0,25)]
+def backtracking():
+    if len(path) == M:
+        print(*path)
+        return
+    else:
+        for i in range(1,N+1):
+            path.append(i)
+            backtracking()
+            path.pop()
 
-diftime = []
-n = int(sys.stdin.readline())
-
-for _ in range(n):
-    st,ed = map(int, sys.stdin.readline().split())
-    diftime.append((st,ed,ed-st))
-
-diftime.sort(key=lambda x:x[2])
-
-for element in diftime:
-    start,end,_ = element[0],element[1],element[2]
-    if start
+if __name__ == "__main__":
+    N,M = map(int, sys.stdin.readline().split())
+    path = []
+    backtracking()
