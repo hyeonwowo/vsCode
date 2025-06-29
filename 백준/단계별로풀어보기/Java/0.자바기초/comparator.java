42d661a5faacd259a758a6223cac42018f0d1120
hyeonwowo <tlsgusdn0806@naver.com>
2025-06-29 19:18:31 +0900

    a

---
+++ 백준/단계별로풀어보기/Java/0.자바기초/comparator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Person {
+    String name;
+    int age;
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class comparator {
+    public static void main(String[] args) {
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(new Person("현우", 25));
+        list.add(new Person("민수", 20));
+        list.add(new Person("영희", 22));
+
+        // 나이 기준 오름차순 정렬
+        Collections.sort(list, new Comparator<Person>() {
+            public int compare(Person p1, Person p2) {
+                return p1.age - p2.age;
+            }
+        });
+
+        for (Person p : list) {
+            System.out.println(p.name + " : " + p.age);
+        }
+    }
+}
