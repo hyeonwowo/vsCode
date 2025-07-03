34819a804cf7750a5a07bb7d582beb7de921f022
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-03 20:18:46 +0900

    a

--- 백준/단계별로풀어보기/Java/1.입출력과사칙연산/2588_곱셈/Main.java
+++ 백준/단계별로풀어보기/Java/1.입출력과사칙연산/2588_곱셈/Main.java
@@ -4,14 +4,17 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
+        String b = sc.next();
 
-        // 숫자 -> 문자열 분리 -> 숫자로 다시 변환
-        // 수학적 방법 사용 (몫과 나머지)
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(a*b);
+        int b1 = b.charAt(2) - '0';
+        int b2 = b.charAt(1) - '0';
+        int b3 = b.charAt(0) - '0';
+
+        System.out.println(a*b1);
+        System.out.println(a*b2);
+        System.out.println(a*b3);
+        System.out.println(a*Integer.parseInt(b));
         sc.close();
     }
 }
+
