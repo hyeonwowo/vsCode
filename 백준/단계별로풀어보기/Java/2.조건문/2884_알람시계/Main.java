78d451715ba8ef7bc2e7bddedafdfdba7129d1ef
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-03 20:23:30 +0900

    a

---
+++ 백준/단계별로풀어보기/Java/2.조건문/2884_알람시계/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+
+        if (min >= 45)
+            if (hour == 0)
+                System.out.println();
+            else
+                System.out.println();
+        else
+            System.out.println();
+        sc.close();
+    }   
+}
