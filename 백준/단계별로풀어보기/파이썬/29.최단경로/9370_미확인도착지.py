5b9cf061e5f0d5c7a6c76bfb4614be5ed27063b5
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-11 16:35:07 +0900

    a

--- 백준/단계별로풀어보기/파이썬/29.최단경로/9370_미확인도착지.py
+++ 백준/단계별로풀어보기/파이썬/29.최단경로/9370_미확인도착지.py
@@ -1,4 +1,4 @@
-import sys
+import sys # g-h 간선을 포함한 경로가 최단거리라면 정답에 포함
 import heapq
 
 class Edge:
@@ -6,23 +6,23 @@ class Edge:
         self.v = v
         self.w = w
         self.weight = weight
-        
+
 class Graph:
     def __init__(self, V):
         self.V = V
         self.adj = [[] for _ in range(self.V+1)]
-        
+
     def addEdge(self, v, w, weight):
         self.adj[v].append(Edge(v, w, weight))
-        
+        self.adj[w].append(Edge(w, v, weight))  # 양방향 도로
+
 def dijkstra(graph, start):
-    distTo = []
-    edgeTo = []
-    visited = []    
+    distTo = [float('inf')] * (graph.V + 1)
+    visited = [False] * (graph.V + 1)
     pq = []
     
     distTo[start] = 0
-    heapq.heappush(pq, ())
+    heapq.heappush(pq, (0, start))
     
     while pq:
         dist, node = heapq.heappop(pq)
@@ -33,11 +33,9 @@ def dijkstra(graph, start):
         for edge in graph.adj[node]:
             if distTo[edge.w] > distTo[edge.v] + edge.weight:
                 distTo[edge.w] = distTo[edge.v] + edge.weight
-                edgeTo[edge.w] = edge.v
-                heapq.heappush(pq, (distTo[edgeTo.w], edge.w))
+                heapq.heappush(pq, (distTo[edge.w], edge.w))
     
-    return distTo, edgeTo
-
+    return distTo
 
 if __name__ == "__main__":
     T = int(sys.stdin.readline())
@@ -46,28 +44,29 @@ if __name__ == "__main__":
         V, E, t = map(int, sys.stdin.readline().split())
         s, g, h = map(int, sys.stdin.readline().split())
         graph = Graph(V)
-        
+
+        weight_gh = None  # g-h 간선 weight 저장
+
         for _ in range(E):
             v, w, weight = map(int, sys.stdin.readline().split())
             graph.addEdge(v, w, weight)
-        
+            if (v == g and w == h) or (v == h and w == g):
+                weight_gh = weight
+
         destnation = []
         for _ in range(t):
             destnation.append(int(sys.stdin.readline()))
-            
-        res = []
-        for i in range(t):
-            e = destnation[i]
-            s_g = dijkstra(graph, s)
-            g_h = dijkstra(graph, g)
-            h_e = dijkstra(graph, h)
-            
-            s_h = dijkstra(graph, s)
-            h_g = dijkstra(graph, h)
-            g_e = dijkstra(graph, g)
-            
-            road_gh = s_g[g] + g_h[h] + h_e[e]
-            road_hg = s_h[h] + h_g[g] + g_e[e]
-            res.append(min(road_gh, road_hg))
-            
-        print(*sorted(res))
+        
+        dist_s = dijkstra(graph, s)
+        dist_g = dijkstra(graph, g)
+        dist_h = dijkstra(graph, h)
+
+        result = []
+        for e in destnation:
+            path1 = dist_s[g] + weight_gh + dist_h[e]
+            path2 = dist_s[h] + weight_gh + dist_g[e]
+            # g-h 간선을 포함한 경로가 실제 최단거리라면 정답
+            if dist_s[e] == path1 or dist_s[e] == path2:
+                result.append(e)
+        
+        print(*sorted(result))
