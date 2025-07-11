584b344b3b2afec006066320e0ac9dbc535dbdea
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-11 16:28:42 +0900

    a

--- 백준/단계별로풀어보기/파이썬/29.최단경로/9370_미확인도착지.py
+++ 백준/단계별로풀어보기/파이썬/29.최단경로/9370_미확인도착지.py
@@ -1,21 +1,73 @@
 import sys
+import heapq
 
 class Edge:
-    pass
-
+    def __init__(self, v, w, weight):
+        self.v = v
+        self.w = w
+        self.weight = weight
+        
 class Graph:
-    pass
+    def __init__(self, V):
+        self.V = V
+        self.adj = [[] for _ in range(self.V+1)]
+        
+    def addEdge(self, v, w, weight):
+        self.adj[v].append(Edge(v, w, weight))
+        
+def dijkstra(graph, start):
+    distTo = []
+    edgeTo = []
+    visited = []    
+    pq = []
+    
+    distTo[start] = 0
+    heapq.heappush(pq, ())
+    
+    while pq:
+        dist, node = heapq.heappop(pq)
+        if visited[node]:
+            continue
+        visited[node] = True
+        
+        for edge in graph.adj[node]:
+            if distTo[edge.w] > distTo[edge.v] + edge.weight:
+                distTo[edge.w] = distTo[edge.v] + edge.weight
+                edgeTo[edge.w] = edge.v
+                heapq.heappush(pq, (distTo[edgeTo.w], edge.w))
+    
+    return distTo, edgeTo
 
-def dijkstra():
-    pass
 
 if __name__ == "__main__":
     T = int(sys.stdin.readline())
     
     for _ in range(T):
-        n, m, t = map(int, sys.stdin.readline().split())
+        V, E, t = map(int, sys.stdin.readline().split())
+        s, g, h = map(int, sys.stdin.readline().split())
+        graph = Graph(V)
         
-        dest = []
+        for _ in range(E):
+            v, w, weight = map(int, sys.stdin.readline().split())
+            graph.addEdge(v, w, weight)
+        
+        destnation = []
         for _ in range(t):
-            dest.append(int(sys.stdin.readline()))
-    
+            destnation.append(int(sys.stdin.readline()))
+            
+        res = []
+        for i in range(t):
+            e = destnation[i]
+            s_g = dijkstra(graph, s)
+            g_h = dijkstra(graph, g)
+            h_e = dijkstra(graph, h)
+            
+            s_h = dijkstra(graph, s)
+            h_g = dijkstra(graph, h)
+            g_e = dijkstra(graph, g)
+            
+            road_gh = s_g[g] + g_h[h] + h_e[e]
+            road_hg = s_h[h] + h_g[g] + g_e[e]
+            res.append(min(road_gh, road_hg))
+            
+        print(*sorted(res))
--- 백준/단계별로풀어보기/파이썬/29.최단경로/연습장.py
+++ 백준/단계별로풀어보기/파이썬/29.최단경로/연습장.py
@@ -1,76 +1,56 @@
 import sys
-
-# 1. 시작 정점의 거리(distTo[start])를 0으로 초기화, 나머지는 ∞로 초기화
-# 2. (V - 1)번 반복:
-#     - 모든 간선 (v → w, weight)에 대해:
-#         - distTo[w] > distTo[v] + weight 만족 시:
-#             → distTo[w] = distTo[v] + weight 로 갱신
-# 3. 모든 간선 (v → w, weight)에 대해 한 번 더 검사:
-#     - 만약 distTo[w] > distTo[v] + weight 여전히 성립한다면
-#         → 음수 사이클 존재 → 실패 처리
-# 4. 성공적으로 종료 시, distTo 배열이 최단 거리
-
+import heapq
 
 class Edge:
     def __init__(self, v, w, weight):
-        self.v = v  # from
-        self.w = w  # to
+        self.v = v
+        self.w = w
         self.weight = weight
-
+        
 class Graph:
     def __init__(self, V):
         self.V = V
-        self.edges = []
+        self.adj = [[] for _ in range(self.V + 1)]
 
     def addEdge(self, v, w, weight):
-        self.edges.append(Edge(v, w, weight))
+        self.adj[v].append(Edge(v, w, weight))
 
-def relax(v, w, weight, distTo, edgeTo):
-    if distTo[v] != float('inf') and distTo[v] + weight < distTo[w]: # 순회하는 위치의 노드를 방문한 적이 없다면 (distTo[v] != float('inf')) 스킵
-        distTo[w] = distTo[v] + weight
-        edgeTo[w] = v
-        return True
-    return False
-
-def bellman_ford(graph, start):
-    V = graph.V
-    edgeTo = [None] * (V+1)
-    distTo[start] = [float('inf')] * (V+1)
-    distTo[start] = 0
+def dijkstra(graph, start):
+    distTo = [float('inf')] * (graph.V+1)
+    edgeTo = [None] * (graph.V+1)
+    visited = [False] * (graph.V+1)
+    pq = []
     
-    for _ in range(V-1):
-        for edge in graph.edges:
-            relax(edge.v, edge.w, edge.weight, distTo, edgeTo)
+    distTo[start] = 0
+    heapq.heappush(pq, (distTo[start], start))
+    
+    while pq:
+        dist, node = heapq.heappop(pq)
+        if visited[node]:
+            continue
+        visited[node] = True
+        
+        for edge in graph.adj[node]:
+            if distTo[edge.w] > distTo[edge.v] + edge.weight:
+                distTo[edge.w] = distTo[edge.v] + edge.weight
+                edgeTo[edge.w] = edge.v
+                heapq.heappush(pq, (distTo[edge.w], edge.w))
     
     return distTo, edgeTo
 
-def pathTo(edgeTo, v):
-    path = []
-    while v is not None:
-        path.append(v)
-        v = edgeTo[v]
-    path.reverse()
-    return path
-
 if __name__ == "__main__":
-    g = Graph(5)
-    g.addEdge(1, 2, 6)
-    g.addEdge(1, 3, 7)
-    g.addEdge(2, 3, 8)
-    g.addEdge(2, 4, 5)
-    g.addEdge(2, 5, -4)
-    g.addEdge(3, 4, -3)
-    g.addEdge(3, 5, 9)
-    g.addEdge(4, 2, -2)
-    g.addEdge(5, 4, 7)
-
-    start = 1
-    distTo, edgeTo = bellman_ford(g, start)
-
-    if distTo is not None:
-        for v in range(1, g.V + 1):
-            path = pathTo(edgeTo, v)
-            if distTo[v] == float('inf'):
-                print(f"{start} -> {v}: 도달 불가")
-            else:
-                print(f"{start} -> {v}: 거리={distTo[v]}, 경로={path}")
+    V, E = map(int, sys.stdin.readline().split())
+    s = int(sys.stdin.readline())
+    graph = Graph(V)
+    
+    for _ in range(E):
+        v, w, weight = map(int, sys.stdin.readline().split())
+        graph.addEdge(v, w, weight)
+        
+    distTo, edgeTo = dijkstra(graph, s)
+    
+    for i in range(1, V+1):
+        if distTo[i] == float('inf'):
+            print("INF")
+        else:
+            print(distTo[i])
