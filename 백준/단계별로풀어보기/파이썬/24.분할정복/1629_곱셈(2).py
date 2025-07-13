2ec9dc7b6effdcc7b8e602784f52aea431cd6052
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-13 14:36:09 +0900

    a

--- 백준/단계별로풀어보기/파이썬/24.분할정복/1629_곱셈(2).py
+++ 백준/단계별로풀어보기/파이썬/24.분할정복/1629_곱셈(2).py
@@ -1,13 +1,15 @@
 import sys
 
-def power(a, n, c):
-    if n == 0: return 1
-    half = power(a, n//2, c)
-    if n % 2 == 0:
-        return (half * half) % c
+def power(a, n):
+    if n == 0:
+        return 1
     else:
-        return (half * half * a) % c
+        half = power(a, n // 2)
+        if n % 2 == 0:
+            return (half * half) % c
+        else:
+            return (half * half * a) % c
 
 if __name__ == "__main__":
     a, n, c = map(int, sys.stdin.readline().split())
-    print(power(a, n, c))
+    print(power(a, n))
--- 백준/단계별로풀어보기/파이썬/24.분할정복/1629_곱셈.py
+++ 백준/단계별로풀어보기/파이썬/24.분할정복/1629_곱셈.py
@@ -1,15 +1,15 @@
 import sys
 
-def power(a,n):
-    if n == 0: # 분할 (기저조건 : 더이상 쪼갤 수 없을때)
+def power(a, n):
+    if n == 0:
         return 1
-    half = power(a, n//2) # 분할
-    if n % 2 == 0:
-        return half * half # 정복
     else:
-        return half * half * a # 정복
+        half = power(a, n // 2)
+        if n % 2 == 0:
+            return (half * half) % C
+        else:
+            return (half * half * a) % C
 
 if __name__ == "__main__":
-    a, n, c = map(int, sys.stdin.readline().split())
-    print(power(a,n) % c)
-    
+    A, B, C = map(int, sys.stdin.readline().split())
+    print(power(A, B))
