259e1b072ce6333fa233ad061b404475e62ec248
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-20 19:44:31 +0900

    a

--- 백준/단계별로풀어보기/파이썬/20.백트래킹/15686_치킨배달.py
+++ 백준/단계별로풀어보기/파이썬/20.백트래킹/15686_치킨배달.py
@@ -1,36 +1,40 @@
 import sys
 
 def distance(chickenlst):
-    mincand = float('inf')
+    total = 0
     for hx, hy in housePoint:
+        mincand = float('inf')
         for cx, cy in chickenlst:
-            mincand = min(mincand, abs(hx-cx) + abs(hy-cy))
-    return mincand
+            mincand = min(mincand, abs(hx - cx) + abs(hy - cy))
+        total += mincand
+    return total
 
 def chicken(start):
+    global minval
     if len(chickenCnt) == m:
         cand = distance(chickenCnt)
         minval = min(minval, cand)
     else:
-        for i in range(start, n):
+        for i in range(start, len(chickenPoint)):
             chickenCnt.append(chickenPoint[i])
-            chicken(i+1)
+            chicken(i + 1)
             chickenCnt.pop()
 
 if __name__ == "__main__":
     n, m = map(int, sys.stdin.readline().split())
-    grid = [list(map(int, sys.stdin.readline().split()))]
-    
-    chickenPoint = []
-    housePoint = []
+    grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+    housePoint = []     # 집 위치
+    chickenPoint = []   # 치킨집 위치
+
     for i in range(n):
         for j in range(n):
             if grid[i][j] == 1:
-                chickenPoint.append((i, j))
-            elif grid[i][j] == 2:
                 housePoint.append((i, j))
-                
+            elif grid[i][j] == 2:
+                chickenPoint.append((i, j))
+
     minval = float('inf')
     chickenCnt = []
-    start = 0 
-    chicken(start)
+    chicken(0)
+    print(minval)
