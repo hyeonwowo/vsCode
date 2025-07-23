d613c6e2c0d5c81249467dbc8962b3670d04fe83
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-23 16:37:46 +0900

    a

--- 백준/단계별로풀어보기/파이썬/31.동적계획법2/14002_가장긴증가하는부분수열4.py
+++ 백준/단계별로풀어보기/파이썬/31.동적계획법2/14002_가장긴증가하는부분수열4.py
@@ -2,17 +2,25 @@ import sys
 
 def mostincrease():
     dp = [1] * n
-    res = []
+    prev = [-1] * n
+    
     for i in range(n):
         for j in range(i):
-            if lst[j] < lst[i]:
-                prev = dp[i]
-                dp[i] = max(dp[i], dp[j]+1)
-                if dp[i] == prev+1:
-                    res.append(lst[i])
-    return res
+            if lst[j] < lst[i] and dp[i] < dp[j] + 1:
+                dp[i] = dp[j] + 1
+                prev[i] = j
+                  
+    idx = dp.index(max(dp))
+    path = []  
+    while idx != -1:
+        path.append(lst[idx])
+        idx = prev[idx]
+
+    return max(dp), list(reversed(path))
 
 if __name__ == "__main__":
     n = int(sys.stdin.readline())
     lst = list(map(int, sys.stdin.readline().split()))
-    print(*mostincrease())
+    maxval, path = mostincrease()
+    print(maxval)
+    print(*path)
