1b6e0d6e2f6fae620a7dcb8952bda9039c8de32b
hyeonwowo <tlsgusdn0806@naver.com>
2025-07-24 21:33:01 +0900

    a

--- 백준/단계별로풀어보기/파이썬/31.동적계획법2/2294_동전2.py
+++ 백준/단계별로풀어보기/파이썬/31.동적계획법2/2294_동전2.py
@@ -2,12 +2,18 @@ import sys
 
 if __name__ == "__main__":
     n, k = map(int, sys.stdin.readline().split())
-    dp = [[float('inf')] * k for _ in range(n)]
-    coin = list(map(int, sys.stdin.readline().split()))
-                
-    for i in range(k):
+    dp = [float('inf')] * (k + 1)
+    coin = [int(sys.stdin.readline()) for _ in range(n)]
+
+    dp[0] = 0  # 초기값: 0원을 만드는 데 드는 동전 수는 0
+
+    for i in range(1, k + 1):
         for j in range(n):
-            pass
-             
+            if coin[j] <= i:
+                dp[i] = min(dp[i], dp[i-coin[j]]+1)
             
-                
+    minval = dp[k]
+    if minval == float('inf'):
+        print(-1)
+    else:
+        print(minval)
