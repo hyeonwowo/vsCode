6abe05bf4bf1e4e5e2b9afe2171341946456ebf2
hyeonwowo <tlsgusdn0806@naver.com>
2025-08-27 17:56:56 +0900

    a

--- 백준/단계별로풀어보기/파이썬/20.백트래킹/12100_2048(EASY).py
+++ 백준/단계별로풀어보기/파이썬/20.백트래킹/12100_2048(EASY).py
@@ -3,91 +3,59 @@ import copy
 sys.setrecursionlimit(10**6)
 
 def left(lst):
-    newlst = [] # 0제거리스트
-    for element in lst:
-        if element != 0:
-            newlst.append(element)
-
     res = []
     i = 0
-    while i < len(newlst):
-        if i+1 < len(newlst) and newlst[i] == newlst[i+1]:
+    while i < len(lst):
+        if i+1 < len(lst) and lst[i] == lst[i+1]:
             res.append(lst[i]+lst[i+1])
             i += 2
         else:
             res.append(lst[i])
             i += 1
+    # 길이가 n보다 짧으면 0 채우기
+    while len(res) < n:
+        res.append(0)
     return res
 
 def right(lst):
-    newlst = [] # 0제거리스트
-    for element in lst:
-        if element != 0:
-            newlst.append(element)
-
-    newlst.reverse(0)
-    res = []
-    i = 0
-    while i < len(newlst):
-        if i+1 < len(newlst) and newlst[i] == newlst[i+1]:
-            res.append(lst[i]+lst[i+1])
-            i += 2
-        else:
-            res.append(lst[i])
-            i += 1
-    return res.reverse()
+    lst.reverse()
+    res = left(lst)
+    res.reverse()
+    return res
 
 def up(lst):
-    newlst = [] # 0제거리스트
-    for element in lst:
-        if element != 0:
-            newlst.append(element)
-
-    res = []
-    i = 0
-    while i < len(newlst):
-        if i+1 < len(newlst) and newlst[i] == newlst[i+1]:
-            res.append(lst[i]+lst[i+1])
-            i += 2
-        else:
-            res.append(lst[i])
-            i += 1
+    res = left(lst)
     return res
 
 def down(lst):
-    newlst = [] # 0제거리스트
-    for element in lst:
-        if element != 0:
-            newlst.append(element)
-
-    newlst.reverse(0)
-    res = []
-    i = 0
-    while i < len(newlst):
-        if i+1 < len(newlst) and newlst[i] == newlst[i+1]:
-            res.append(lst[i]+lst[i+1])
-            i += 2
-        else:
-            res.append(lst[i])
-            i += 1
-    return res.reverse()
+    lst.reverse()
+    res = left(lst)
+    res.reverse()
+    return res
 
 def moveblock(dx, dy, board):
-    if dx == -1 and dy == 0: # 좌측방향
-        
-        
-        
-    elif dx == 1 and dy == 0: # 우측방향
-
-
-
-    elif dx == 0 and dy == -1: # 아래방향
-
+    if dy == 0:
+        res = []
+        for i in range(n):
+            row = [board[i][j] for j in range(n) if board[i][j] != 0]
+            if dx == -1:
+                newrow = left(row)
+            else:
+                newrow = right(row)
+            res.append(newrow)
+        return res
     
-    else: # 위방향
-
-
-        
+    else:  
+        res = [[0]*n for _ in range(n)]
+        for j in range(n):
+            col = [board[i][j] for i in range(n) if board[i][j] != 0]
+            if dy == -1:
+                newcol = down(col)
+            else:
+                newcol = up(col)
+            for i in range(n):
+                res[i][j] = newcol[i]
+        return res
 
 def backtracking(cnt, board):
     global maxval
@@ -98,13 +66,13 @@ def backtracking(cnt, board):
         return
     else:
         for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            
-        
+            copyboard = copy.deepcopy(board)
+            moveboard = moveblock(dx, dy, copyboard)
+            backtracking(cnt+1, moveboard)
 
 if __name__ == "__main__":
     n = int(sys.stdin.readline())
     grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
     maxval = 0
-    
-    backtracking(1, grid) 
+    backtracking(0, grid)
     print(maxval)
--- 백준/단계별로풀어보기/파이썬/20.백트래킹/연습장.py
+++ 백준/단계별로풀어보기/파이썬/20.백트래킹/연습장.py
@@ -1,21 +1,4 @@
-lst = [2,0,0,2,2,0,4]
-res = []
-
-# 일단은 0 제거
-newlst = []
-for element in lst:
-    if element != 0:
-        newlst.append(element)
-print(newlst)
-
-lst = [2,2,4,2,4] # 좌측방향으로 합치기 -> [4,4,2,4]
-
-i = 0
-while i < len(lst):
-    if i+1 < len(lst) and lst[i] == lst[i+1]:
-        res.append(lst[i]+lst[i+1])
-        i += 2
-    else:
-        res.append(lst[i])
-        i += 1
-print(res)
+lst = []
+ls = [1,2,3]
+lst.append(ls)
+print(lst)
