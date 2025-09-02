89c031a0897008e83603cc210596aac54257e02e
hyeonwowo <tlsgusdn0806@naver.com>
2025-09-02 19:03:17 +0900

    a

--- 백준/단계별로풀어보기/파이썬/35.최소신장트리/2887_행성터널(2).py
+++ 백준/단계별로풀어보기/파이썬/35.최소신장트리/2887_행성터널(2).py
@@ -1,4 +1,4 @@
-import sys # 메모리초과
+import sys
 import heapq
 
 def prim(V, adj):
@@ -21,49 +21,45 @@ def prim(V, adj):
             if not visited[w] and weight < dist[w]:
                 dist[w] = weight
                 heapq.heappush(heap, (weight, w))
-    
+                
     return total_weight
-        
+
 if __name__ == "__main__":
     V = int(sys.stdin.readline())
-    
     P = []
+    
     for _ in range(V):
         x, y, z = map(int, sys.stdin.readline().split())
         P.append((x, y, z))
-    
-    xadj = []
-    yadj = []
-    zadj = []
-    for i in range(V):
-        for j in range(i+1, V):
-            xadj.append((abs(P[i][0] - P[j][0]), i, j))
-            yadj.append((abs(P[i][1] - P[j][1]), i, j))
-            zadj.append((abs(P[i][2] - P[j][2]), i, j))
-    
-    xadj.sort()
-    yadj.sort()
-    zadj.sort()
-    
+
     adj = [[] for _ in range(V)]
-    for i in range(V-1): 
-        weight, v, w = xadj[i]
-        adj[v].append((w, weight))
-        adj[w].append((v, weight))
-        
-        weight, v, w = yadj[i]
-        adj[v].append((w, weight))
-        adj[w].append((v, weight))
-        
-        weight, v, w = zadj[i]
+
+    # x좌표를 인덱스와 함께 저장
+    PX = [(P[i][0], i) for i in range(V)]
+    PX.sort()
+    for i in range(V-1):
+        weight = abs(PX[i][0] - PX[i+1][0])
+        v, w = PX[i][1], PX[i+1][1]
         adj[v].append((w, weight))
         adj[w].append((v, weight))
     
-    
+    # y좌표를 인덱스와 함꼐 저장
+    PY = [(P[i][1], i) for i in range(V)]
+    PY.sort()
+    for i in range(V-1):
+        weight = abs(PY[i][0] - PY[i+1][0])
+        v, w = PY[i][1], PY[i+1][1]
+        adj[v].append((w, weight))
+        adj[w].append((v, weight))
+        
+    # z좌표를 인덱스와 함께 저장
+    PZ = [(P[i][2], i) for i in range(V)]
+    PZ.sort()
+    for i in range(V-1):
+        weight = abs(PZ[i][0] - PZ[i+1][0])
+        v, w = PZ[i][1], PZ[i+1][1]
+        adj[v].append((w, weight))
+        adj[w].append((v, weight))
+        
     total_weight = prim(V, adj)
     print(total_weight)
-    # 1, 모든 간선 구하기
-    # 2, V-1 만큼의 간선 추리기 (sort())
-    # 3. adj 업데이트
-    # 4. 크루스칼 돌리기
-    
