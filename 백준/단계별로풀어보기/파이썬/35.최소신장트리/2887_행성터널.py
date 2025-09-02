61562e792a7978447b9eb43fa6ced7c3a0a84762
hyeonwowo <tlsgusdn0806@naver.com>
2025-09-02 17:40:40 +0900

    a

--- 백준/단계별로풀어보기/파이썬/35.최소신장트리/2887_행성터널.py
+++ 백준/단계별로풀어보기/파이썬/35.최소신장트리/2887_행성터널.py
@@ -20,16 +20,50 @@ def prim(V, adj):
         for w, weight in adj[v]:
             if not visited[w] and weight < dist[w]:
                 dist[w] = weight
-                heapq.heappush(heap, (w, weight))
+                heapq.heappush(heap, (weight, w))
     
     return total_weight
         
 if __name__ == "__main__":
     V = int(sys.stdin.readline())
     
-    P = [[] for _ in range(V)]
+    P = []
     for _ in range(V):
         x, y, z = map(int, sys.stdin.readline().split())
         P.append((x, y, z))
+    
+    xadj = []
+    yadj = []
+    zadj = []
+    for i in range(V):
+        for j in range(i+1, V):
+            xadj.append((abs(P[i][0] - P[j][0]), i, j))
+            yadj.append((abs(P[i][1] - P[j][1]), i, j))
+            zadj.append((abs(P[i][2] - P[j][2]), i, j))
+    
+    xadj.sort()
+    yadj.sort()
+    zadj.sort()
+    
+    adj = [[] for _ in range(V)]
+    for i in range(V-1): 
+        weight, v, w = xadj[i]
+        adj[v].append((w, weight))
+        adj[w].append((v, weight))
         
+        weight, v, w = yadj[i]
+        adj[v].append((w, weight))
+        adj[w].append((v, weight))
+        
+        weight, v, w = zadj[i]
+        adj[v].append((w, weight))
+        adj[w].append((v, weight))
+    
+    
+    total_weight = prim(V, adj)
+    print(total_weight)
+    # 1, 모든 간선 구하기
+    # 2, V-1 만큼의 간선 추리기 (sort())
+    # 3. adj 업데이트
+    # 4. 크루스칼 돌리기
     
--- 백준/단계별로풀어보기/파이썬/35.최소신장트리/새롭게알게된.py
+++ 백준/단계별로풀어보기/파이썬/35.최소신장트리/새롭게알게된.py
@@ -5,3 +5,17 @@ print(f"{value:.2f}")  # 3.14
 
 # 간선이 많으면 프림, 적으면 크루스칼
 # E ~= V*V : prim, E ~= V : kruskal
+
+
+# sorted(), .sort()
+lst = [5,3,4,2,1]
+
+lst.sort() # 기존 리스트를 정렬된 상태로 바꾼다
+print(lst) # [1,2,3,4,5]
+
+
+lst = [5,3,4,2,1]
+
+new = sorted(lst) # 복사된 정렬 리스트르 반환, 기존 리스트는 변함 없음
+print(lst) # [5,3,4,2,1]
+print(new) # [1,2,3,4,5]
