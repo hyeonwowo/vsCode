a63279cdfec6f4194da0c28fa21cf4cebfd12fa5
hyeonwowo <tlsgusdn0806@naver.com>
2025-09-27 11:40:27 +0900

    a

---
+++ 백준/단계별로풀어보기/파이썬/29.최단경로/5719_거의최단경로.py
@@ -0,0 +1,64 @@
+import sys
+import heapq
+
+# 최단경로 구하기
+# 최단경로 엣지를 graph에서 지우기
+# graph에서 다시 최단경로 구하기
+# 끝.
+
+class Edge:
+    def __init__(self, v, w, weight):
+        self.v = v
+        self.w = w
+        self.weight = weight
+        
+class Graph:
+    def __init__(self, V):
+        self.V = V
+        self.adj = [[] for _ in range(V)]
+    
+    def addEdge(self, v, w, weight):
+        self.adj[v].append(Edge(v, w, weight))
+        
+def relax(v, w, weight, edgeTo, distTo, pq):
+    if distTo[v] + weight < distTo[w]:
+        distTo[w] = distTo[v] + weight
+        edgeTo[w] = v
+        heapq.heappush(pq, (distTo[w], w))
+
+def dijkstra(start, graph):
+    V = graph.V
+    edgeTo = [None] * V
+    distTo = [float('inf')] * V
+    pq = []
+    
+    distTo[start] = 0
+    heapq.heappush(pq, (0, start))
+    
+    while pq:
+        curr_dist, v = heapq.heappop(pq)
+        if distTo[v] < curr_dist:
+            continue
+        
+        for edge in graph.adj[v]:
+            relax(edge.v, edge.w, edge.weight, edgeTo, distTo, pq)
+            
+    return edgeTo, distTo
+
+def pathTo(start, g):
+    
+
+if __name__ == "__main__":
+    res = []
+    while True:
+        n, m = map(int, sys.stdin.readline().split())
+        if n == 0 and m == 0:
+            break
+        
+        start, end = map(int, sys.stdin.readline().split())
+        g = Graph(n)
+        for _ in range(m):
+            v, w, weight = map(int, sys.stdin.readline().split())
+            g.addEdge(v, w, weight)
+            
+        edgeTo, distTo = dijkstra(start, g)
