588c38ec3d53c11d2ca68bfb2df457c567478e92
hyeonwowo <tlsgusdn0806@naver.com>
2025-09-27 11:45:43 +0900

    a

--- 백준/단계별로풀어보기/파이썬/29.최단경로/5719_거의최단경로.py
+++ 백준/단계별로풀어보기/파이썬/29.최단경로/5719_거의최단경로.py
@@ -66,4 +66,14 @@ if __name__ == "__main__":
             v, w, weight = map(int, sys.stdin.readline().split())
             g.addEdge(v, w, weight)
             
-        edgeTo, distTo = dijkstra(start, g)
+        edgeTo, _ = dijkstra(start, g)
+        path = pathTo(edgeTo, end)
+        
+        for v, w in path:
+            g.adj[v].remove(w)
+          
+        _, distTo = dijkstra(start, g)  
+        res.append(distTo[end])
+        
+    for ele in res:
+        print(ele)
